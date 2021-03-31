8ac208683429dab9e1965bb715fa0ad1ece62fbf
Szerednik László Tamás <hsq25l@INF.ELTE.HU>
2021-03-31 22:12:08 +0200

    pictures to 200x200 from 180x180

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = props => (
     <div className='card'>
-        <img alt='kitty' src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`} />
+        <img alt='kitty' src={`https://robohash.org/${props.monster.id}?set=set4&size=200x200`} />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
